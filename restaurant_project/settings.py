af89ca358b9bb9da658c4245acaa8de2477735fa
Chandan kumar (Chandan Kushwaha) <156070216+chandan9546@users.noreply.github.com>
2025-06-21 20:49:05 +0530

    Update settings.py
    Signed-off-by: Chandan kumar (Chandan Kushwaha) <156070216+chandan9546@users.noreply.github.com>

--- restaurant_project/settings.py
+++ restaurant_project/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['restaurant-website-ior0.onrender.com', '127.0.0.1', 'localhost']
 
